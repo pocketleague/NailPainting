f1b0acb35dcc03d8cae7839ef8dfd8bd9647c66c
Siddhesh Dhopatkar <siddho2010@gmail.com>
2020-08-03 15:42:15 +0530

    no message

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    public GameObject nail;
+
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().sharedMaterials[1].SetColor("_Color", Color.red);
+    }
+}
