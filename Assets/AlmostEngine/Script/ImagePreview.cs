e015dd82d82a2beaf3610377af3903e6bd54d28f
Siddhesh Dhopatkar <siddho2010@gmail.com>
2020-09-25 13:34:41 +0530

    ScreenshotCompanion added

---
+++ Assets/AlmostEngine/Script/ImagePreview.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImagePreview : MonoBehaviour
+{
+	
+	public AlmostEngine.Examples.ValidationCanvas validateCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    Invoke("Capture",0.5f);
+    }
+
+
+	public void Capture()
+	{
+		validateCanvas.Capture();
+		validateCanvas.OnSaveCallback();
+	}
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+	    
+    }
+}
